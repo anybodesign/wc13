0654822108bb7b180ef7f9c3385496e90f0aa0fd
thomas <atelier@anybodesign.com>
2017-09-06 16:18:30 +0200

    Contrast

--- js/main.js
+++ js/main.js
@@ -43,6 +43,13 @@ jQuery(document).ready(function($) {
 	});
 	
 	
+	// Transcript
+	
+	$('.show-transcript').click(function() {
+		$('.transcript').slideToggle();
+		
+		return false;
+	});
 	
 	
 	// Set focus
@@ -77,7 +84,46 @@ jQuery(document).ready(function($) {
 	  
   
   
-  
+	// Contrast
+	
+
+    // Check (onLoad) if the cookie is there and set the class if it is
+    
+    if ($.cookie('highcontrast') === "yes") {
+        $("body").addClass("highcontrast");
+    }
+    
+    // When the element is clicked
+   
+    $("a.toggle-highcontrast").click(function () {
+        
+        if ($.cookie('highcontrast') === "undefined" || $.cookie('highcontrast') === "no") {
+           
+            $.cookie('highcontrast', 'yes', {
+                expires: 7,
+                path: '/'
+            });
+            $("body").addClass("highcontrast");
+        
+        } else {
+
+            $.cookie('highcontrast', 'yes', {
+                expires: 7,
+                path: '/'
+            });
+            $("body").addClass("highcontrast");
+            
+        }
+    });
+    
+    $("a.toggle-remove").click(function () {
+        $('body').removeClass('highcontrast');
+        if ($.cookie('highcontrast') === "yes") {
+            $.cookie("highcontrast", null, {
+                path: '/'
+            });
+        }
+    });	
   
   
   /**	
